4027bfba34fe754008d901008eca3cb66dc0cddf
YouK-Kim <2010232073@yonsei.ac.kr>
2021-03-15 12:11:19 +0900

    Basic Setting 210315/12:11

---
+++ src/kr/co/softsoldesk/config/ServletAppContext.java
@@ -0,0 +1,29 @@
+package kr.co.softsoldesk.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//servlet-context.xml의 역할을 하는 클래스
+@Configuration
+@EnableWebMvc //Controller어노테이션이 설정된 클래스를 자동 등록
+@ComponentScan("kr.co.softsoldesk.controller")
+public class ServletAppContext implements WebMvcConfigurer{
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+}
